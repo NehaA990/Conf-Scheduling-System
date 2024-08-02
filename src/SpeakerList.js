f8f317b04722f177a44c6bc34b4ef814b0eccc4e
NehaA990 <neharora990@gmail.com>
2024-08-02 14:12:35 -0700

    css project

--- src/SpeakerList.js
+++ src/SpeakerList.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 function SpeakerList() {
   return (
-    <div>
+    <div className="speaker-list">
       <h2>Speaker List</h2>
       <ul>
-        <li>Speaker 1</li>
-        <li>Speaker 2</li>
-        <li>Speaker 3</li>
+        <li>
+          <span>Speaker 1</span>
+          <button>View Bio</button>
+        </li>
+        <li>
+          <span>Speaker 2</span>
+          <button>View Bio</button>
+        </li>
+        <li>
+          <span>Speaker 3</span>
+          <button>View Bio</button>
+        </li>
       </ul>
     </div>
   );
